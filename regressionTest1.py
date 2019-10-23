ca40aff08becd9ef758c51eb2e83dc9c808d3e84
luciaemiliaeu <luciaemilia72@gmail.com>
2019-10-23 11:24:12 -0300

    Ajuste polinomial da função do erro

--- regressionTest1.py
+++ regressionTest1.py
@@ -21,10 +21,10 @@ for dataset, n_clusters in datasets:
 
 	# Cria DataFrame com os valores de X e o cluster Y
 	dataset = pd.read_csv(dataset, sep=',',parse_dates=True)
-	classe = dataset.loc[:,'classe'].get_values()
-	cluster = dataset.drop('classe', axis=1)
-	atributos_names = cluster.columns
-	cluster = pd.DataFrame(MinMaxScaler().fit_transform(cluster.get_values()), columns = atributos_names)
+	Y = dataset.loc[:,'classe'].get_values()
+	X = dataset.drop('classe', axis=1)
+	atributos_names = X.columns
+	cluster = pd.DataFrame(MinMaxScaler().fit_transform(X.get_values()), columns = atributos_names)
 	
 	
 	for attr in range(cluster.shape[1]):
@@ -43,7 +43,7 @@ for dataset, n_clusters in datasets:
 		predicted = svr_rbf.fit(X_train,y_train).predict(X_test)
 		
 		# Dataframe : {y_real, y_Predicted, Classe, [attr]}
-		clt = classe[test_set.index]
+		clt = Y[test_set.index]
 		y_ = pd.DataFrame({'Actual': y_test, 'Predicted': predicted, 'Cluster': clt})
 		y_ = y_.assign(Erro=lambda x: abs(x.Actual-x.Predicted))
 		y_.index = test_set.index
@@ -56,12 +56,23 @@ for dataset, n_clusters in datasets:
 		fig.suptitle(atributos_names[attr])
 		figE.suptitle(atributos_names[attr])
 
+		
 		# Separa os dados em grupos
 		for c, data in y_.groupby(['Cluster']):
 			data = data.sort_values(by=atributos_names[attr])
 			erro_relativo = data.loc[:,'Erro']
-			attr_column = data.loc[:,atributos_names[attr]]
 			
+			q = X.loc[:, atributos_names[attr]]
+			q = q[data.index]
+			attr_column = q.sort_values().get_values()
+
+			print(attr_column)
+			poli = np.polyfit(attr_column, erro_relativo, 4)
+			xx = np.linspace(min(attr_column), max(attr_column))
+			axesE[c-1].plot(xx, np.polyval(poli, xx))
+			#axesE[c-1].set_xtickslabels(attr_column)
+			
+			'''
 			end = np.shape(attr_column)[0]
 			x = np.linspace(0, end, end, endpoint = True)
 			interpolacao_linear = interp1d(x, erro_relativo, kind='linear')
@@ -71,14 +82,14 @@ for dataset, n_clusters in datasets:
 			qua = interpolacao_quadratic(x)
 			cub = interpolacao_cubic(x)
 
+			lines = axesE[c-1].plot(x, erro_relativo, x, lin, x, qua, x, cub , 'k--')
+			plt.setp(lines[0], color='b')
+			plt.setp(lines[1], color ='y')
+			plt.setp(lines[2], color = 'g')
+			plt.legend(('linear', 'quadratic', 'cubic'))'''
 
-			axesE[c-1].plot(x, erro_relativo, x, lin, 'k--', legend)
-			axesE[c-1].plot(x, erro_relativo, x, qua, 'k--')
-			axesE[c-1].plot(x, erro_relativo, x, cub, 'k--')
-			
-
-			#x = np.arange(data.shape[0])
-			#axesE[c-1].plot(x, erro_relativo)
+			x = np.arange(min(attr_column), max(attr_column))
+			axesE[c-1].plot(attr_column, erro_relativo)
 			
 			data = data[['Actual', 'Predicted', 'Erro']]			
 			data.plot(kind = 'bar', ax=axes[c-1], title=('Classe '+str(c)))
